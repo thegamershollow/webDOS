5303e250feb22e0272460990905b9dacfdb83270
TheGamersHollow <thegamershollow@outlook.com>
2024-12-09 10:06:33 +0000

    make comment changes and add code

--- webDOS.ino
+++ webDOS.ino
@@ -22,6 +22,7 @@ void setup() {
     btnAState = digitalRead(USER_BUTTON_A); // get button A state and set it to variable
     btnBState = digitalRead(USER_BUTTON_B); // get button B state and set it to variable
     Screen.print("LOADING..."); // print loading to the screen
+    delay(100) // set a 100 ms delay
     // check for the presence of the shield:
     if (WiFi.status() == WL_NO_SHIELD) {
         Serial.println("WiFi shield not present");
@@ -78,13 +79,13 @@ void loop() {
                     client.println("<head>");
                     client.println("<meta charset='UTF-8'>");
                     client.println("<title>webDOS</title>"); // set webpage title
-                    client.println("<style>html,body{height:100%;} #dos {width: 100%; height: 100%;}</style>");
-                    client.println("<link rel='stylesheet' href='https://v8.js-dos.com/latest/js-dos.css'>");
-                    client.println("<script src='https://v8.js-dos.com/latest/js-dos.js'></script>");
+                    client.println("<style>html,body{height:100%;} #dos {width: 100%; height: 100%;}</style>"); // set width and height of the window
+                    client.println("<link rel='stylesheet' href='https://v8.js-dos.com/latest/js-dos.css'>"); // load the js-dos css style sheet
+                    client.println("<script src='https://v8.js-dos.com/latest/js-dos.js'></script>"); // load the js-dos javascript file
                     client.println("</head>");
                     client.println("<body>");
-                    client.println("<div id='dos' style='width: 100%; height: 100%'></div>");
-                    client.println("<script>Dos(document.getElementById('dos'), {url: 'https://raw.githubusercontent.com/thegamershollow/webDOS/refs/heads/main/bundle.jsdos',});</script>");
+                    client.println("<div id='dos' style='width: 100%; height: 100%'></div>"); // set width and height to 100%
+                    client.println("<script>Dos(document.getElementById('dos'), {url: 'https://raw.githubusercontent.com/thegamershollow/webDOS/refs/heads/main/bundle.jsdos',});</script>"); // load the js-dos package
                     client.println("</body>");
                     client.println("</html>");
                     break;
@@ -102,31 +103,28 @@ void loop() {
         delay(1);
         // close the connection:
         client.stop();
-        Serial.println("client disonnected");
-        digitalWrite(LED_USER, LOW);
+        Serial.println("client disonnected"); // print status to serial
+        digitalWrite(LED_USER, LOW); // set led to off state
     }
 }
 void printWifiStatus() {
-    // print the SSID of the network you're attached to:
-    Serial.print("SSID: ");
-    Serial.println(WiFi.SSID());
-    // print your WiFi shield's IP address:
-    IPAddress ip = WiFi.localIP();
+    IPAddress ip = WiFi.localIP(); // get the ip address of the webserver running on the arduino
+    // convert the ip address into a c string
     uint8_t IP_array[4]={ip[0],ip[1],ip[2],ip[3]};
     String strIP= String(IP_array[0])+"."+String(IP_array[1])+"."+String(IP_array[2])+"."+String(IP_array[3]);
     const char* ipCStr = strIP.c_str();
-    Screen.clean();
-    Serial.print("WiFi SSID: ");
-    Serial.println(WiFi.SSID());
-    Serial.print("IP Address: ");
-    Serial.println(strIP);
-    Serial.print("Port: ");
-    Serial.println(port);
-    Screen.print(0, "WiFi SSID:");
-    Screen.print(1, WiFi.SSID());
-    Screen.print(2,"IP Address:");
-    Screen.print(3, ipCStr, true);
-    digitalWrite(LED_AZURE, HIGH);
+    Screen.clean(); // clear screen
+    Serial.print("WiFi SSID: "); // print wifi ssid to serial
+    Serial.println(WiFi.SSID()); // print wifi ssid to serial
+    Serial.print("IP Address: "); // print ip address to serial
+    Serial.println(strIP); // print ip address to serial
+    Serial.print("Port: "); // print port to serial
+    Serial.println(port); // print port to serial
+    Screen.print(0, "WiFi SSID:"); // print wifi ssid to screen
+    Screen.print(1, WiFi.SSID()); // print wifi ssid to screen
+    Screen.print(2,"IP Address:"); // print ip address to screen
+    Screen.print(3, ipCStr, true); // print ip address to screen
+    digitalWrite(LED_AZURE, HIGH); // set the AZURE led to high
     // print the received signal strength:
     long rssi = WiFi.RSSI();
     Serial.print("signal strength (RSSI):");
